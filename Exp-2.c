2eeb83be0308509752857c95e0a8f3fd6b2e2ec8
sanskriti-singh03 <Sanskriti.25603@stu.upes.ac.in>
2025-10-21 15:13:10 +0530

    Add C program to print address in multiple lines

---
+++ Exp-2.c
@@ -0,0 +1,9 @@
+//Write a C Program to print the address in multiple lines (new line).
+#include <stdio.h>
+
+int main() {
+    printf("G4,block4,Express Garden\n");
+    printf("Vaibhav Khand,Indirapuram\n");
+    printf("Ghaziabad,201014\n");
+    return 0;
+}
