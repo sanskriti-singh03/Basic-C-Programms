05dc7720c5e7d01a14a39797645e65d8e7935d78
sanskriti-singh03 <Sanskriti.25603@stu.upes.ac.in>
2025-10-21 15:14:21 +0530

    Create a user input program for name and age
    This program prompts the user for their name and age, then displays a greeting.

---
+++ Exp-3.c
@@ -0,0 +1,13 @@
+//Write a program that prompts the user to enter their name and age.
+#include <stdio.h>
+
+int main() {
+    char name[50]; 
+    int age;       
+    printf("Enter your name: ");
+    scanf("%s", name);
+    printf("Enter your age: ");
+    scanf("%d", &age);
+    printf("Hello, %s! You are %d years old.\n", name, age);
+     return 0;
+}
